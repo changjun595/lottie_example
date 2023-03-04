33198abbb5045b1dae5e990d1f2338e577cd66fc
ROBOFRIEN <33312259+changjun595@users.noreply.github.com>
2023-03-04 20:06:05 +0900

    Initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.cyanAccent,
+        title: Text('Lottie Example'),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Lottie.network('https://assets7.lottiefiles.com/packages/lf20_KEahK5k9Mf.json')),
+    );
+  }
+}
